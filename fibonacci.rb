81596e5e894ec87d2c5026dc358bffa5257b5c14
Marcin Galiński <marcin.galinski95@gmail.com>
2015-12-29 01:16:47 +0100

    create fibs_rec method
    variant of fibs method using recursion instead of loop

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,5 +1,16 @@
 def fibs num
 	f = [0, 1]
 	(num - 2).times { f.push(f[-2] + f[-1]) }
-	puts f.first(num).join(' ')
+	f.first(num)
+end
+
+def fibs_rec num
+	case num
+		when 0 then []
+		when 1 then [0]
+		when 2 then [0, 1]
+		else 
+			prev = fibs_rec(num - 1)
+			prev << prev[-1] + prev[-2]
+	end
 end
