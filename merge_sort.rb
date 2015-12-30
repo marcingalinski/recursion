3bc004739bbe39fff7331fdb52faf5b9b1e3f143
Marcin Galiński <marcin.galinski95@gmail.com>
2015-12-30 22:32:18 +0100

    refactor code

--- merge_sort.rb
+++ merge_sort.rb
@@ -1,12 +1,9 @@
 def merge_sort array
 	return array if array.size <= 1
-	a = merge_sort(array[0...(array.length / 2)])
-	b = merge_sort(array[(array.length / 2)..-1])
+	half = array.size / 2
+	a = merge_sort array[0...half]
+	b = merge_sort array[half..-1]
 	result = Array.new(array.size) do |_i|
-		if b.empty? || a.any? && a.first < b.first
-			a.slice!(0) 
-		else 
-			b.slice!(0)
-		end 
+		(b.empty? || a.any? && a.first < b.first ? a : b).slice!(0)
 	end
 end
